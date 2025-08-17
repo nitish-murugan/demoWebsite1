3e5fc700d848c5d96814772d7e2a8448ff6f2a20
nitish-murugan <nitishmurugan2006@gmail.com>
2025-08-17 11:23:49 +0530

    added host

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0', // Allows external access
     port: 5173, // Uses the environment port or defaults to 5173
-    allowedHosts: ['demowebsite1.onrender.com/'],
+    allowedHosts: ['demowebsite1.onrender.com'], // No trailing slash
   }
 });
