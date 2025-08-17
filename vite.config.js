71f8d870cd431e189e1ef39fce535057ef1c7621
nitish-murugan <nitishmurugan2006@gmail.com>
2025-08-17 11:18:06 +0530

    init3

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
-export default {
+export default defineConfig({
+  plugins: [react()],
   server: {
     host: '0.0.0.0', // Allows external access
-    port: process.env.PORT || 5173, // Uses the environment port or defaults to 5173
+    port: 5173, // Uses the environment port or defaults to 5173
   }
-};
-
+});
